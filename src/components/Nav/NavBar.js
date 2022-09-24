8fd203eb29882fa3139d89b66a14b613f43b246f
ashiqurras <ashiqurrussel2@gmail.com>
2022-09-24 23:06:41 +0200

    navbar created

---
+++ src/components/Nav/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "./NavBar.css";
+const NavBar = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+
+      <div className="list">
+        <a href="/shop">Shop</a>
+        <a href="/orders">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/home">Home</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
